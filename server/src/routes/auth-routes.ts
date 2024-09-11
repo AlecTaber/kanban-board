df8ba2491e8e5a56c93cb13a17416dc3ba3ff258
Alec Taber <alectaber12@gmail.com>
2024-09-11 14:43:49 -0700

    fix authorization errors

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,29 +4,28 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
   const { username, password } = req.body;
+
   const user = await User.findOne({
-    where: { username: username }, // Simple object notation
+    where: { username },
   });
-
   if (!user) {
-    return res.status(404).send('User not found');
+    return res.status(401).json({ message: 'Authentication failed' });
   }
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword) {
-    return res.status(401).send('Invalid password');
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
   }
-  const secretKey = process.env.JWT_SECRET_KEY || 'secret';
 
-  const token = jwt.sign ({ id: user.id }, secretKey, { expiresIn: '1h' });
+  const secretKey = process.env.JWT_SECRET_KEY || '';
 
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
   return res.json({ token });
 };
 
 const router = Router();
 
-// POST /login - Login a user
 router.post('/login', login);
 
-export default router;
+export default router
